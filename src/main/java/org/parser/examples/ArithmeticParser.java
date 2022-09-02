4a19fbafc9c5901fca26a22e3f02996ceaf8a55a
Frederik Böcker <fredboe@outlook.com>
2022-09-02 18:40:25 +0200

    Added new Tests and extended the arithmetic parser

--- src/main/java/org/parser/examples/ArithmeticParser.java
+++ src/main/java/org/parser/examples/ArithmeticParser.java
@@ -1,11 +1,34 @@
 package org.parser.examples;
 
+import org.parser.Consumable;
+import org.parser.base.Parser;
 import org.parser.base.build.ParserBuilder;
 import org.parser.base.build.ParserPool;
+import org.parser.tree.AST;
 
-public class ArithmeticParser {
+import java.util.Optional;
+
+public class ArithmeticParser<ANNOTATION> implements Parser<ArithmeticParser.TYPE, ANNOTATION> {
     public enum TYPE {
-        NUMBER, ADD, SUB, MUL, DIV
+        NUMBER, ADD, SUB, MUL, DIV, POT, FUNC, SIN, COS, TAN, PI, E
+    }
+
+    private final Parser<TYPE, ANNOTATION> aritParser;
+
+    public ArithmeticParser() {
+        aritParser = ArithmeticParser.<ANNOTATION>arithmeticExample().getParser("EXPR");
+    }
+
+    @Override
+    public Optional<AST<TYPE, ANNOTATION>> applyTo(Consumable consumable) {
+        return aritParser.applyTo(consumable);
+    }
+
+    @Override
+    public Optional<AST<TYPE, ANNOTATION>> applyTo(CharSequence sequence) {
+        return aritParser.applyTo(
+                new Consumable(sequence, Consumable.Ignore.IGNORE_WHITESPACE, Consumable.Ignore.IGNORE_LINEBREAK)
+        );
     }
 
     /**
@@ -15,15 +38,23 @@ public class ArithmeticParser {
      * ADD ::= SUB ("+" SUB)+ | SUB <br>
      * SUB ::= MUL ("-" MUL)+ | MUL <br>
      * MUL ::= DIV ("*" DIV)+ | DIV <br>
-     * DIV ::= BRAC ("/" BRAC)+ | BRAC <br>
-     * BRAC ::= "(" EXPR ")" | NUMBER <br>
+     * DIV ::= POT ("/" POT)+ | POT <br>
+     * POT ::= SUBEXPR ("^" SUBEXPR)+ | SUBEXPR <br>
+     * SUBEXPR ::= BRAC | VAL
+     * VAL ::= NUMBER | FUNC_SYMBOl BRAC | CONST
+     * BRAC ::= "(" EXPR ")" <br>
+     * FUNC_SYMBOL ::= "sin" | "cos" | "tan"
+     * CONST ::= "pi" | "e"
      * @return Returns a ParserPool for arithmetic expressions.
      * @param <ANNOTATION> ANNOTATION type of the AST
      */
     public static <ANNOTATION> ParserPool<TYPE, ANNOTATION> arithmeticExample() {
         ParserBuilder<TYPE, ANNOTATION> builder = new ParserBuilder<>();
 
-        builder.newRule("NUMBER").consistsOf().match(TYPE.NUMBER, "\\d+").end();
+        // optional - then some digits, then optional . with digits and then optional exponent starting with e or E, optional +/- and then some digits
+        builder.newRule("NUMBER").consistsOf()
+                .match(TYPE.NUMBER, "(\\-)?\\d+(\\.\\d*)?((e|E)(\\+|\\-)?\\d+)?")
+                .end();
 
         builder.newRule("ADD").consistsOf()
                 .concat(TYPE.ADD).rule("SUB").some().match("\\+").rule("SUB").someEnd()
@@ -44,16 +75,39 @@ public class ArithmeticParser {
                 .end();
 
         builder.newRule("DIV").consistsOf()
-                .concat(TYPE.DIV).rule("BRAC").some().match("/").rule("BRAC").someEnd()
+                .concat(TYPE.DIV).rule("POT").some().match("/").rule("POT").someEnd()
                 .or()
-                .rule("BRAC")
+                .rule("POT")
+                .end();
+
+        builder.newRule("POT").consistsOf()
+                .concat(TYPE.POT).rule("SUBEXPR").some().match("\\^").rule("SUBEXPR").someEnd()
+                .or()
+                .rule("SUBEXPR")
+                .end();
+
+        builder.newRule("SUBEXPR").consistsOf()
+                .rule("BRAC").or().rule("VAL").end();
+
+        builder.newRule("VAL").consistsOf()
+                .rule("NUMBER")
+                .or()
+                .concat(TYPE.FUNC).rule("FUNC_SYMBOL").rule("BRAC")
+                .or()
+                .rule("CONST")
                 .end();
 
         builder.newRule("BRAC").consistsOf()
-                .concat().match("\\(").rule("EXPR").many("\\)")
-                .or()
-                .rule("NUMBER")
+                .concat().match("\\(").rule("EXPR").match("\\)")
                 .end();
+
+        builder.newRule("FUNC_SYMBOL").consistsOf()
+                .match(TYPE.SIN, "sin").or()
+                .match(TYPE.COS, "cos").or()
+                .match(TYPE.TAN, "tan").end();
+
+        builder.newRule("CONST").consistsOf()
+                        .match(TYPE.PI, "pi").or().match(TYPE.E, "e").end();
 
         builder.newRule("EXPR").consistsOf().rule("ADD").end();
 
---
+++ src/test/java/org/parser/ArithmeticParserTest.java
@@ -0,0 +1,138 @@
+package org.parser;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.parser.base.Parser;
+import org.parser.examples.ArithmeticParser;
+import org.parser.tree.AST;
+import org.parser.examples.ArithmeticParser.TYPE;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class ArithmeticParserTest {
+    enum ANNOTATION {}
+
+    private final Parser<TYPE, ANNOTATION> exprParser = new ArithmeticParser<>();
+
+    private AST<TYPE, ANNOTATION> setupASTofExpr1() {
+        // 42 + 11 - 1*20/10-14
+        AST<TYPE, ANNOTATION> num42 = new AST<>(TYPE.NUMBER, new Consumable.Match("42"));
+        AST<TYPE, ANNOTATION> num11 = new AST<>(TYPE.NUMBER, new Consumable.Match("11"));
+        AST<TYPE, ANNOTATION> num1  = new AST<>(TYPE.NUMBER, new Consumable.Match("1"));
+        AST<TYPE, ANNOTATION> num20 = new AST<>(TYPE.NUMBER, new Consumable.Match("20"));
+        AST<TYPE, ANNOTATION> num10 = new AST<>(TYPE.NUMBER, new Consumable.Match("10"));
+        AST<TYPE, ANNOTATION> num14 = new AST<>(TYPE.NUMBER, new Consumable.Match("14"));
+
+        AST<TYPE, ANNOTATION> divAST = new AST<>(TYPE.DIV, null, List.of(num20, num10));
+        AST<TYPE, ANNOTATION> mulAST = new AST<>(TYPE.MUL, null, List.of(num1, divAST));
+        AST<TYPE, ANNOTATION> subAST = new AST<>(TYPE.SUB, null, List.of(num11, mulAST, num14));
+
+        return new AST<>(TYPE.ADD, null, List.of(num42, subAST));
+    }
+
+    private AST<TYPE, ANNOTATION> setupASTofExpr2() {
+        // 11*2+24/2*3 - 9*2
+        AST<TYPE, ANNOTATION> num11 = new AST<>(TYPE.NUMBER, new Consumable.Match("11"));
+        AST<TYPE, ANNOTATION> num2  = new AST<>(TYPE.NUMBER, new Consumable.Match("2"));
+        AST<TYPE, ANNOTATION> num24 = new AST<>(TYPE.NUMBER, new Consumable.Match("24"));
+        AST<TYPE, ANNOTATION> num3  = new AST<>(TYPE.NUMBER, new Consumable.Match("3"));
+        AST<TYPE, ANNOTATION> num9  = new AST<>(TYPE.NUMBER, new Consumable.Match("9"));
+
+        AST<TYPE, ANNOTATION> divAST = new AST<>(TYPE.DIV, null, List.of(num24, num2));
+        AST<TYPE, ANNOTATION> mulAST1 = new AST<>(TYPE.MUL, null, List.of(divAST, num3));
+        AST<TYPE, ANNOTATION> mulAST2 = new AST<>(TYPE.MUL, null, List.of(num9, num2));
+        AST<TYPE, ANNOTATION> subAST = new AST<>(TYPE.SUB, null, List.of(mulAST1, mulAST2));
+        AST<TYPE, ANNOTATION> mulAST3 = new AST<>(TYPE.MUL, null, List.of(num11, num2));
+
+        return new AST<>(TYPE.ADD, null, List.of(mulAST3, subAST));
+    }
+
+    private AST<TYPE, ANNOTATION> setupASTofExpr3() {
+        // sin(14+3)*3^2- 1
+        AST<TYPE, ANNOTATION> sin   = new AST<>(TYPE.SIN, new Consumable.Match("sin"));
+        AST<TYPE, ANNOTATION> num14 = new AST<>(TYPE.NUMBER, new Consumable.Match("14"));
+        AST<TYPE, ANNOTATION> num3  = new AST<>(TYPE.NUMBER, new Consumable.Match("3"));
+        AST<TYPE, ANNOTATION> num2  = new AST<>(TYPE.NUMBER, new Consumable.Match("2"));
+        AST<TYPE, ANNOTATION> num1  = new AST<>(TYPE.NUMBER, new Consumable.Match("1"));
+
+        AST<TYPE, ANNOTATION> addAST = new AST<>(TYPE.ADD, null, List.of(num14, num3));
+        AST<TYPE, ANNOTATION> sinAST = new AST<>(TYPE.FUNC, null, List.of(sin, addAST));
+        AST<TYPE, ANNOTATION> potAST = new AST<>(TYPE.POT, null, List.of(num3, num2));
+        AST<TYPE, ANNOTATION> mulAST = new AST<>(TYPE.MUL, null, List.of(sinAST, potAST));
+
+        return new AST<>(TYPE.SUB, null, List.of(mulAST, num1));
+    }
+
+    private AST<TYPE, ANNOTATION> setupASTofExpr4() {
+        // 12 - 11 - (pi - 2^1*2)
+        AST<TYPE, ANNOTATION> num12 = new AST<>(TYPE.NUMBER, new Consumable.Match("12"));
+        AST<TYPE, ANNOTATION> num11 = new AST<>(TYPE.NUMBER, new Consumable.Match("11"));
+        AST<TYPE, ANNOTATION> num2  = new AST<>(TYPE.NUMBER, new Consumable.Match("2"));
+        AST<TYPE, ANNOTATION> num1  = new AST<>(TYPE.NUMBER, new Consumable.Match("1"));
+        AST<TYPE, ANNOTATION> pi = new AST<>(TYPE.PI, new Consumable.Match("pi"));
+
+        AST<TYPE, ANNOTATION> potAST = new AST<>(TYPE.POT, null, List.of(num2, num1));
+        AST<TYPE, ANNOTATION> mulAST = new AST<>(TYPE.MUL, null, List.of(potAST, num2));
+        AST<TYPE, ANNOTATION> subAST = new AST<>(TYPE.SUB, null, List.of(pi, mulAST));
+
+        return new AST<>(TYPE.SUB, null, List.of(num12, num11, subAST));
+    }
+
+    private AST<TYPE, ANNOTATION> setupASTofExpr5() {
+        // 9 - 5 - 1 + 6 - 5-1
+        AST<TYPE, ANNOTATION> num9 = new AST<>(TYPE.NUMBER, new Consumable.Match("9"));
+        AST<TYPE, ANNOTATION> num5 = new AST<>(TYPE.NUMBER, new Consumable.Match("5"));
+        AST<TYPE, ANNOTATION> num1  = new AST<>(TYPE.NUMBER, new Consumable.Match("1"));
+        AST<TYPE, ANNOTATION> num6  = new AST<>(TYPE.NUMBER, new Consumable.Match("6"));
+
+        AST<TYPE, ANNOTATION> subASTl = new AST<>(TYPE.SUB, null, List.of(num9, num5, num1));
+        AST<TYPE, ANNOTATION> subASTr = new AST<>(TYPE.SUB, null, List.of(num6, num5, num1));
+
+        return new AST<>(TYPE.ADD, null, List.of(subASTl, subASTr));
+    }
+
+    @Test
+    public void Test_expr1_string() {
+        String expr1 = "42 + 11 - 1*20/10-14";
+        var optionalAST = exprParser.applyTo(expr1);
+        assertTrue(optionalAST.isPresent());
+        assertEquals(optionalAST.get(), setupASTofExpr1());
+    }
+
+    @Test
+    public void Test_expr2_string() {
+        String expr2 = "11*2+24/2*3 - 9*2";
+        var optionalAST = exprParser.applyTo(expr2);
+        assertTrue(optionalAST.isPresent());
+        assertEquals(optionalAST.get(), setupASTofExpr2());
+    }
+
+    @Test
+    public void Test_expr3_string() {
+        String expr3 = "sin(14+3)*3^2- 1";
+        var optionalAST = exprParser.applyTo(expr3);
+        assertTrue(optionalAST.isPresent());
+        assertEquals(optionalAST.get(), setupASTofExpr3());
+    }
+
+    @Test
+    public void Test_expr4_string() {
+        String expr4 = "12 - 11 - (pi - 2^1*2)";
+        var optionalAST = exprParser.applyTo(expr4);
+        assertTrue(optionalAST.isPresent());
+        assertEquals(optionalAST.get(), setupASTofExpr4());
+    }
+
+    @Test
+    public void Test_expr5_string() {
+        String expr5 = "9 - 5 - 1 + 6 - 5-1";
+        var optionalAST = exprParser.applyTo(expr5);
+        assertTrue(optionalAST.isPresent());
+        assertEquals(optionalAST.get(), setupASTofExpr5());
+    }
+}
